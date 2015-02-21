3ea4e46b03cc06dfa24c7d8cf61ea69aa44a5c98
biswajit <biswajit@biswajit-Inspiron-3542>
2015-02-22 00:33:30 +0530

    xxxx

--- src/com/ws/client/invoke/WSInvokeStrutr.java
+++ src/com/ws/client/invoke/WSInvokeStrutr.java
@@ -5,9 +5,15 @@ import java.lang.reflect.Method;
 
 public class WSInvokeStrutr {
 	
-	public Object invokeWS(String className, String operation, Class[] inputClass, Object[] input) throws ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException{
+	public Object invokeWS(String className, String operation, Object[] input) throws ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException{
 		
 		Class<?> concreteClass = Class.forName(className);
+		Class[] inputClass = new Class[input.length];
+		for(int i=0;i<input.length;i++){
+			inputClass[i] = input[i].getClass();
+			System.out.println(inputClass[i]);
+		}
+		System.out.println("teset");
 		Method m = concreteClass.getDeclaredMethod(operation, inputClass);
 		Object output = m.invoke(concreteClass.newInstance(), input);
 		return output;
--- src/com/ws/client/main/WSClientMain.java
+++ src/com/ws/client/main/WSClientMain.java
@@ -3,6 +3,7 @@ package com.ws.client.main;
 import java.lang.reflect.InvocationTargetException;
 
 import com.ws.client.invoke.WSInvokeStrutr;
+import com.ws.model.Employee;
 import com.ws.model.Person;
 
 public class WSClientMain {
@@ -10,11 +11,12 @@ public class WSClientMain {
 	public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException {
 		WSInvokeStrutr invoke = new WSInvokeStrutr();
 		Person person = new Person();
+		Employee emp = new Employee();
 		person.setName("biswa");
 		person.setAddr("hindmotor");
 		String className = "com.ws.service.PerosnServiceImplProxy";
 		String operationName = "searchPerson";
-		Person personOutput = (Person)invoke.invokeWS(className, operationName, new Class[]{com.ws.model.Person.class}, new Object[]{person});
+		Person personOutput = (Person)invoke.invokeWS(className, operationName, new Object[]{person});
 
 		System.out.println(person.getName());
 	}
